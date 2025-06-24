adc4d067e4ed70ff58c12f2b093f0eaefd6475f8
deansaril <deanvsaril@gmail.com>
2025-06-24 18:20:38 +0800

    Initial commit

---
+++ src/test/java/TestClass.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestClass {
+
+    @Test
+    public void TestMethod()
+    {
+        assertTrue(true);
+    }
+
+}
